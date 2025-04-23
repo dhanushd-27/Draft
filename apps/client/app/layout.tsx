9b40c6e241966cda2dabeb450cef729771429607
dhanushd-27 <dhanushsai2005@gmail.com>
2025-04-23 17:56:39 +0530

    Add: Turborepo

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -2,14 +2,10 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-});
+const courierPrimeRegular = localFont({
+  src: "./fonts/Courier_Prime/CourierPrime-Regular.ttf",
+  variable: "--font-courier-prime-regular"
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${ courierPrimeRegular }`}>
         {children}
       </body>
     </html>
