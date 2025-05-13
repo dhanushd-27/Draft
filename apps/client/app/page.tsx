e68ffd0ed00a058715501be50b30fe73c02143dc
Turbobot <turbobot@vercel.com>
2025-05-13 19:04:18 +0530

    feat(create-turbo): apply official-starter transform

--- apps/client/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Landing() {
-  return (
-    <div className='min-h-screen bg-neutral-100 flex flex-col items-center justify-center'>
-      Landing Page
-    </div>
-  )
-}
